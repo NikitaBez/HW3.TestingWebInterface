9ce0d6d11eebdb208d5e07df9f0f030fa6f9eb59
NikitaBez <bezmenov.nikita@gmail.com>
2022-10-28 23:20:09 +0900

    Modified test, added webdriver for linux

--- src/test/java/ru/netology/web/DebitCardTest.java
+++ src/test/java/ru/netology/web/DebitCardTest.java
@@ -22,8 +22,13 @@ public class DebitCardTest {
 
     @BeforeAll
     static void setUpAll(){
-        System.setProperty("webdriver.chrome.driver","driver/win10/chromedriver.exe");
+        if (System.getProperty("os.name").contains("Linux")) {
+            System.setProperty("webdriver.chrome.driver", "driver/linux/chromedriver");
+        } else {
+            System.setProperty("webdriver.chrome.driver", "driver/win10/chromedriver.exe");
+        }
     }
+
 
     @BeforeEach
     void setUp(){
